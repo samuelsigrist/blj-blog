e7925b4f87fa2b31f4ef65d24cc210959bdbcedd
samuelsigrist <samuel_sigrist@outlook.com>
2018-11-28 14:43:59 +0100

    worked on A001, A002

--- Blog.php
+++ Blog.php
@@ -10,81 +10,49 @@
     <body>  
         <h1>Blog von Samuel Sigrist</h1>
 
-        <?php
-          $first_name  = $_POST['vorname']    ?? '';
-            $second_name = $_POST['nachname']   ?? '';
-                $post        = $_POST['post']   ?? '';
-                ?>
-                <form action="Blog.php" method="post">
+        <?php 
+            $user = "root";
+            $pw = "";
+            $con = new PDO('mysql:host=localhost;dbname=blogdb', $user, $pw);
 
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-            <fieldset>
-                <legend class="form-legend">Blogeintrage</legend>
-                <div>
-                    <p> Blog eintrag </p>
-                    Blogeintrag von <?php echo htmlspecialchars($_POST['name']); ?><br>
-                    Eintrag:<?php echo $_POST['Blog']; ?><br>
-                    <!--Der Eintrag wurde am <?php echo ((int)$_POST['Datum']); ?> veröffentlicht.-->
-                </div>
-            </fieldset>
+                $createdBy  = $_POST['name']    ?? '';
+                $title = $_POST['post-title']   ?? '';
+                $post = $_POST['post-text']   ?? '';
+                /* echo $createdBy . '/' . $postTitle . '/' . $post; */
 
-            <fieldset>
+                $sql = "INSERT INTO posts (created_by, created_at,  post_title, post_text)"; 
+                $sql .= " VALUES('" . $createdBy . "', Now(),'" . $title . "', '" . $post. "')";
+
+                $con->exec($sql);
+            }
+        ?>
+
+        <form action="Blog.php" method="post">
+                <fieldset>
                 <legend class="form-legend">Poste einen Blog eintrag</legend>
                 <div 
                     action="action.php" method="post">
                     <p>Ihr Name: <input type="text" name="name" required/></p>
-                    <textarea cols="40" rows="5"name="Blog">Ihr Blog eintrag</textarea>
+                    <p>Titel: <input type="text" name="post-title" required/></p>
+                    <textarea cols="40" rows="5"name="post-text">Ihr Blog eintrag</textarea>
+                    <p>Bild einfügen</p>
                     <p><input type="submit" /></p> 
                 </div>               
             </fieldset>
         </form>
+
+        <?php 
+            $stmt = $con->query('SELECT * FROM posts');
+            foreach($stmt->fetchAll() as $row) {
+               
+                echo 'Blogpost von:  ' . $row['created_by'] . '<br>';
+                echo 'Titel:  ' . $row['post_title'] . '<br>';
+                echo  $row['post_text'];
+                echo '<hr>';
+            }
+        ?>
+
     </body>
-</html>
-
-<?php  /*
-$servername = "localhost";
-$user = "root";
-$pw = "";
-$db = "blogdb";
-
-$con = new mysqli($servername, $user, $pw, $db);
-
-if($con->connect_error){
-    die("Fehler".$con->connect_error);
-}
-$sql = "INSERT INTO User (Name, Passwort)  VALUES ('Samuel SIgrist', 'es funktioniert');
-
-if ($con->query($sql) === TRUE){
-    while($i = $res->fetch_assoc)
-    echo "Die Anmeldung war erfolgreich";
-}
-else {
-    echo "Anmeldung fehlgeschlagen" . $con->error;
-}
-$con->close();
-?>
-
-
-
-<?php
-$servername = "localhost";
-$user = "root";
-$pw = "";
-$db = "blogdb";
-
-$con = new mysqli($servername, $user, $pw, $db);
-
-if($con->connect_error){
-    die("Fehler".$con->connect_error);
-}
-$sql = "INSERT INTO User (Name, Passwort)  VALUES ('Samuel SIgrist', 'es funktioniert');
-
-if ($con->query($sql) === TRUE){
-    while($i = $res->fetch_assoc)
-    echo "Die Anmeldung war erfolgreich";
-}
-else {
-    echo "Du bist ein Lappen" . $con->error;
-}
-$con->close();
-?> */
+</html> 
