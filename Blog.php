24486ed28de882e20eb90874d1094c1c8da017ed
samuelsigrist <samuel_sigrist@outlook.com>
2018-11-30 16:06:01 +0100

    V3

--- Blog.php
+++ Blog.php
@@ -14,8 +14,7 @@
             $user = "root";
             $pw = "";
             $con = new PDO('mysql:host=localhost;dbname=blogdb', $user, $pw);
-            if($_SERVER['REQUEST_METHOD'] === 'POST') 
-            {
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $createdBy  = $_POST['name']    ?? '';
                 $title = $_POST['post-title']   ?? '';
                 $post = $_POST['post-text']   ?? '';
@@ -42,16 +41,19 @@
             </fieldset>
         </form>
         <?php   $stmt = $con->query('SELECT * FROM posts');
-                foreach($stmt->fetchAll() as $row)
-            {
+                foreach($stmt->fetchAll() as $row){
                 echo 'Blogpost von:  ' . $row['created_by'] . '<br>';
                 echo 'Titel:  ' . $row['post_title'] . '<br>';
                 echo  $row['post_text'] . '<br>';
                 echo 'Veröffentlicht am: ' . $row['created_at']. '<br>';
-                echo '<img src="' . $row['link']  . '" alt="Bild" onError="this.src="https://www.transparenttextures.com/patterns/asfalt-light.png";>';
-                echo '<hr>';   
-            }
-       ?>
+                echo '<img src="' . $row['link']  . '" alt="Bild" onError="this.src="https://images.obi.ch/product/DE/1500x1500/609120_2.jpg";>';
+                echo '<hr>';}
+                
+        ?>
+        
+        <form method="post" action="/php/BLJ-Blog/Bloglinks.php">
+            <button class="btn" type="submit">Weitere Blogs</button>
+        </form>
     </div>   
     </body>
 </html> 
---
+++ Bloglinks.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Blog.css">
+</head>
+<body>
+    <div class="wrapper">
+
+        <h1 class="form-title">Weitere BLJ Blogs</h1>
+        <p>Das sind die persöndlichen Blogs von den andern</p>
+        <div class="form-actions">
+            <a href="http://10.20.16.101/blog/">Nick</a>
+        </div>
+                <div class="form-actions">
+            <a href="http://10.20.16.106/blog/home/">Leo</a>
+        </div>
+                <div class="form-actions">
+            <a href="http://10.20.16.105/blog/">Philip</a>
+        </div>
+                <div class="form-actions">
+            <a href="http://10.20.16.104/php/blogpost/blog/">Daniel</a>
+        </div>
+                <div class="form-actions">
+            <a href="http://10.20.16.107/blog/blog/">Colin</a>
+        </div>
+                <div class="form-actions">
+            <a href="http://10.20.16.112/blog/">David</a>
+        </div>
+                <div class="form-actions">
+            <a href="http://10.20.16.102/blog-git/">Noah</a>
+        </div>
+        
+            <div class="form-actions">
+                <form method="get" action="blog.php">
+                    <button class="btn" type="submit">Zurück zum Blog</button>
+                </form>
+            </div>
+
+        </form>
+    </div>
+</body>
+</html>
